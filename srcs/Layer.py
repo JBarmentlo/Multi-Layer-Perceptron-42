fixed softmax

srcs/Layer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from types import new_class
 from numpy import matmul, transpose
 import numpy as np
 from activations import get_activation_function
@@ -9,6 +10,7 @@ import logging
 float_formatter = "{:5.2f}".format
 np.set_printoptions(formatter={'float_kind':float_formatter})
 
+logger = logging.getLogger('spam_application')
 
 # TODO: Change docstrings to represent bias units addition
 class Layer():
@@ -65,15 +67,17 @@ class Layer():
 
             returns
         '''
-        # print(f"Backwards : {self.activation.__name__}")
+        logging.debug(f"Backwards : {self.activation.__name__}")
         dadz = self.activation_derivative(self.z, self.a)
         # print(f"{dadz.shape = }, {djda.shape = }")
+        logging.debug(f"djda:\n {djda}\n")
         djdz = np.einsum( 'ik,ikj->ij', djda, dadz)
-        logging.debug(f"{djdz = }")
+        logging.debug(f"djdz:\n {djdz}\n")
         djdw = matmul(self.x.T, djdz)
-        logging.debug(f"{djdw = }")
+        logging.debug(f"djdw:\n{djdw}\n")
         # print(f"{dadz.shape = }, {djdz.shape = }, {djdw.shape = }, {self.w[1:, :].T.shape = }, {self.w.T.shape = }, {self.x.shape = }")
         next_djda = matmul(djdz, self.w[1:, :].T)
+        logging.debug(f"{next_djda.shape = } \nnext_djda:  \n{next_djda * 10}\n\n")
         # print("OUT")
         return next_djda, djdw
 

srcs/activations.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.special import softmax, expit
+import logging
 
+epsilon = 0.00001
 
 def sigmoid(z):
     # a = 1/(1 + np.exp(-z))
@@ -23,10 +25,30 @@ def softmax_col(z):
     s = np.sum(e, axis = 0, keepdims=True)
     return (e/s)
 
+
+def identity(z):
+    return (z)
+
+
+def identyty_derivative(z, a):
+    b, n = a.shape
+    # print("da sig\n", da)
+    # print(f"{da[0] =} , \n{da[1] = }")
+    da = np.einsum('ij,jk->ijk' , np.ones(a.shape), np.eye(n, n))
+    return da
+
+
 def softmax_row(z):
-    e = np.exp(z - np.max(z))
+    # logging.debug(f"z.shape: {z.shape}\nz:\n{z}\nnp.nax.z:\n{z.max(axis=1)}\n")
+    # e = np.exp(z)
+    z = z - z.max(axis = 1, keepdims=True)
+    # logging.debug(f"z:\n{z}\n")
+    e = np.exp(z)
+    # logging.debug(f"e:\n{e}")
     s = np.sum(e, axis = 1, keepdims=True)
-    return (e/s)
+    # logging.debug(f"s : {s}")
+    return (e/(s))
+
 
 def softmax_row_derivative(z, a):
     '''
@@ -52,5 +74,7 @@ def get_activation_function(activation):
             return sigmoid, sigmoid_derivative
         if activation == 'softmax':
             return softmax_row, softmax_row_derivative
+        if activation == "identity":
+            return identity, identyty_derivative
         print("You have entered an incorrect activation function name, defaulting to softmax")
         return softmax_row, softmax_row_derivative